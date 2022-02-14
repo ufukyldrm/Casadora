3e4f460802a685fd1221454c9dcdda48552d064e
ufukyldrm <cufukyldrm@gmail.com>
2022-02-15 00:56:58 +0300

    firstcommit2

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,8 +11,12 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-community/picker
+import com.reactnativecommunity.picker.RNCPickerPackage;
 // @react-native-masked-view/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
+// @react-native-picker/picker
+import com.reactnativecommunity.picker.RNCPickerPackage;
 // lottie-react-native
 import com.airbnb.android.react.lottie.LottiePackage;
 // react-native-camera
@@ -74,7 +78,9 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new RNCPickerPackage(),
       new RNCMaskedViewPackage(),
+      new RNCPickerPackage(),
       new LottiePackage(),
       new RNCameraPackage(),
       new RNGestureHandlerPackage(),
